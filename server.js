1ffd37210945e0a36fa568af3fb6e71dcdaace48
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-15 17:42:48 +0800

    Deleted vlans

--- server.js
+++ server.js
@@ -1,14 +1,5 @@
 mgmt_ip: 10.0.2.2
 sdn_controller: 10.0.0.2
-vlans:
-    green: 
-        id: 10
-    blue
-        id: 20
-    yellow
-        id: 30
-    brown
-        id: 22
 ports:
 1: [10]
 3: [10,20]
