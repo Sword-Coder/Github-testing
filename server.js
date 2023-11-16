a87bedccddff20db462dec9858bc46a766954ede
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-16 15:13:36 +0800

    Added servers and removed lans ports.

--- server.js
+++ server.js
@@ -1,18 +1,4 @@
 mgmt_ip: 10.0.2.2
 sdn_controller: 10.0.0.2
-vlans:
-    yellow
-        id: 59
-    grey
-        id: 10
-    maroon
-        id: 22
-
-    green
-        id: 40
-    blue
-        id: 122
-ports:
-auth_server: 10.40.4.4
 private_net: 20.20.1.2
 
