6ab0ae04970190a7ff6c568cfa5c2af1e69f8fe7
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-16 14:50:02 +0800

    Changed the colors

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 mgmt_ip: 10.0.2.2
 sdn_controller: 10.0.0.2
 vlans:
-    yellow
+    green
         id: 40
-    brown
+    blue
         id: 122
 ports:
-1: [10]
-2: [15]
-4: [10, 20]
 auth_server: 10.40.4.4
 private_net: 20.20.1.2
 
