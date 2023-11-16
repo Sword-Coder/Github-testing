d5798bf4c43f433d86960f2e468be6fc52d6df8c
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-16 14:15:58 +0800

    Removed ports and vlans

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 mgmt_ip: 10.0.2.2
 sdn_controller: 10.0.0.2
 vlans:
-    green: 
-        id: 10
-    blue
-        id: 20
     yellow
         id: 30
     brown
         id: 22
-ports:
-1: [10]
-2: [25]
-3: [10,20]
-4: [10, 20]
-auth_server: 10.30.3.3
