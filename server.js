8c964b20c1c4b4fb4af7fba90a16cadc4ce81de4
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-16 14:17:20 +0800

    Changed details about auth_server and vlans numbers.

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ vlans:
     green: 
         id: 10
     blue
-        id: 20
+        id: 12
     yellow
         id: 30
     brown
         id: 22
 ports:
 1: [10]
-2: [25]
+2: [15]
 3: [10,20]
 4: [10, 20]
-auth_server: 10.30.3.3
+auth_server: 10.40.4.4
+
